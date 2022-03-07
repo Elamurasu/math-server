aab462fb249fc80f8caa99b1e9d5dad6d7188b87
Elamurasu <elamurasu1606@gmail.com>
2022-03-07 19:22:26 +0530

    connect thread

--- process_thread.py
+++ process_thread.py
@@ -3,3 +3,11 @@ class ProcessOutputThread(Thread): #OOPS
 		Thread.__init__(self)
 		self.proc = proc
 		self.conn = conn
+
+	def run(self):
+		while not self.proc.stdout.closed and not self.conn._closed:
+			try:
+				self.conn.sendall(self.proc.stdout.readline())
+			except:
+				pass
+
